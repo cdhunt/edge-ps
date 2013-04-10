861864fcd1b8d95b062646e6d1bcd6168147b151
Chris Hunt <cdhunt@gmail.com>
2013-04-10 09:33:31 -0400

    Ported server.js sample from edge-powershell

---
+++ samples/tryPowerShell/server.js
@@ -0,0 +1,35 @@
+var edge = require('edge');
+var express = require('express');
+
+var app = express();
+var port = process.env.PORT || 8081;
+
+function wrapScript(script) {
+	return '.\\' + script + '.ps1';
+}
+
+var invokePS = edge.func('ps', function() {/*
+#param($fromJS)
+#    "PowerShell [$(Get-Date)]"
+#"PowerShell welcomes $($args[1])"
+"PowerShell welcomes $($fromJS)"
+*/});
+
+app.get('/powershell/:script', function(req,res) {
+
+	var payload = {
+		source: wrapScript(req.params.script),
+		inputFromJs: req.query
+		//request: req,
+		//response: res,
+	}
+	
+	invokePS(payload, function (error, results) {
+		if (error) return console.log(error);
+		//console.log(results.Result);
+		res.send(results.Result);
+	});
+});
+
+app.listen(port);
+console.log("Server listening on port " + port);
